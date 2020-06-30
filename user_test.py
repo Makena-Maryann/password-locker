953127cc001490763b95368134f83a8b7d52a984
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-30 05:42:49 +0300

    Create setup and teardown for the user test class

---
+++ user_test.py
@@ -0,0 +1,23 @@
+import unittest
+from user import User
+
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases.
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Makena", "Makena99*")
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
