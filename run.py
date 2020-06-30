9c52576a0dad874ff63bc03db7b1f2968e16f09b
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-30 04:40:37 +0300

    Create a main function for the credentials

--- run.py
+++ run.py
@@ -3,6 +3,95 @@ from user import User
 from credentials import Credentials
 
 
+def create_credential(aname, uname, password):
+    '''
+    Function to create a new credential
+    '''
+    new_credentials = Credentials(aname, uname, password)
+    return new_credentials
+
+
+def save_credential(credential):
+    '''
+    Function to save credentials
+    '''
+    credential.save_credentials()
+
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credentials()
+
+
+def display_credential():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+
+
+def creds_list():
+    print("Hi there, Welcome to your credentials list.")
+
+    print("What would you like to do?")
+    print('\n')
+
+    while True:
+        print("Please use these codes to state what you want: cc - create a new credential, dc - display all the available credentials, del - delete a credential, ex - exit the credentials list")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print("New Credential")
+            print("-"*10)
+
+            print("Account name ....")
+            a_name = input()
+
+            print("Username ....")
+            u_name = input()
+
+            print("Password ....")
+            passcode = input()
+
+            save_credential(create_credential(a_name, u_name, passcode))
+            print('\n')
+            print(f"{a_name} account credentials saved")
+            print("\n")
+
+        elif short_code == 'dc':
+            if display_credential():
+                print("Here is a list of all your credentials")
+                print('\n')
+
+                for credential in display_credential():
+                    print(
+                        f"{credential.account_name} {credential.user_name} {credential.password}")
+
+                    print('\n')
+
+            else:
+                print('\n')
+                print("You don't seem to have any credentials saved yet")
+                print('\n')
+
+        elif short_code == 'del':
+            print("Enter the name of the account you want to delete.")
+
+            delete_account = input()
+
+            del_credential(delete_account)
+
+        elif short_code == "ex":
+            print("Bye ........")
+            break
+
+        else:
+            print("Wrong short code. Please try again.")
+
+
 def main():
     print("Hello, Welcome to your password locker.")
 
